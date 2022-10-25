86b093fdda6ac2de1e5eaf6748b668995fa09177
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-24 21:17:03 -0300

    feat: add url de pesquisa de cidades

--- api/Controllers/mini-ecommerce.js
+++ api/Controllers/mini-ecommerce.js
@@ -1,8 +1,21 @@
+const cidadeEstado = require('../cidades-estados.json');
+
 function finalizarCompra(req, res) {
     console.log(req.body);
     res.send('ok');
 }
 
+function obterCidadePorEstado(req, res) {
+    const siglaEstado = req.params['siglaEstado'].toUpperCase();
+    const dadosEstado = cidadeEstado.estados.filter(estado => estado.sigla === siglaEstado);
+
+    if (dadosEstado.length === 0)
+        res.status(404).json({ erro: `${siglaEstado} não é um estado válido.` });
+
+    res.json([dadosEstado[0].cidades]);
+}
+
 module.exports = {
-    finalizarCompra
+    finalizarCompra,
+    obterCidadePorEstado
 }
--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const cors = require('cors');
 
-const { finalizarCompra } = require('./Controllers/mini-ecommerce');
+const {
+    finalizarCompra,
+    obterCidadePorEstado
+} = require('./Controllers/mini-ecommerce');
 
 const app = express();
 const port = 3001;
@@ -12,5 +15,6 @@ app.use(bodyParse.json());
 
 // URLs
 app.post('/mini-ecommerce/checkout/finalizar-compra', finalizarCompra);
+app.get('/mini-ecommerce/estado/:siglaEstado/cidades', obterCidadePorEstado);
 
 app.listen(port, () => console.log(`Servidor online in port ${port}`));
