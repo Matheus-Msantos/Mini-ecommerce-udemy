4eca01b8aa98c5bffaaa668c5b8a5c5de2e982d8
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-26 20:10:27 -0300

    feat: mensagem add produto no carrinho

--- front-end/src/components/produtos/Produtos.js
+++ front-end/src/components/produtos/Produtos.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import ListarProdutos from './listar-produtos';
+import React, { useState } from 'react';
 
-function Produtos() {
-    return (
-        <ListarProdutos />
-    );
+import PropTypes from 'prop-types';
+
+import ListarProdutos from './listar-produtos';
+import { Alert } from 'react-bootstrap';
+
+function Produtos(props) {
+
+  const [exibirMsg, setExibirMsg] = useState(false);
+  const [produto, setProduto] = useState('');
+
+  function exibirMensagem(produto) {
+    setExibirMsg(true);
+    setProduto(produto);
+
+    setTimeout(() => {
+      setExibirMsg(false);
+    }, 3000);
+  }
+
+  function visivel() {
+    return props.visivel ? null : 'hidden';
+  }
+
+  return (
+    <div className={visivel()}>
+      <Alert
+        className="mx-2"
+        variant="success"
+        show={exibirMsg}>
+        <b>{produto}</b> adicionado com sucesso ao carrinho
+      </Alert>
+
+      <ListarProdutos
+        exibirMensagem={exibirMensagem}
+      />
+    </div>
+  );
+}
+
+Produtos.propTypes = {
+  visivel: PropTypes.bool.isRequired,
+  adicionarProduto: PropTypes.func.isRequired
 }
 
 export default Produtos;
--- front-end/src/components/produtos/listar-produtos.js
+++ front-end/src/components/produtos/listar-produtos.js
@@ -1,48 +1,54 @@
 import React from 'react';
 import placeholder from '../../assets/images/img-produto.png';
 import { Card, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-function ListarProdutos() {
+function ListarProdutos(props) {
 
-    const produtos = [
-        { nome: 'Aprenda Java', preco: 'R$ 59,90' },
-        { nome: 'JavaScript em 24 horas', preco: 'R$ 19,99' },
-        { nome: 'React em 7 dias', preco: 'R$ 29,99' },
-        { nome: 'Algoritmos e Estrutura de Dados', preco: 'R$ 35,90' },
-        { nome: 'Testes Unitários com Jasmine', preco: 'R$ 14,90' },
-        { nome: 'TypeScript na prática', preco: 'R$ 9,90' }
-    ];
+  const produtos = [
+    { nome: 'Aprenda Java', preco: 'R$ 59,90' },
+    { nome: 'JavaScript em 24 horas', preco: 'R$ 19,99' },
+    { nome: 'React em 7 dias', preco: 'R$ 29,99' },
+    { nome: 'Algoritmos e Estrutura de Dados', preco: 'R$ 35,90' },
+    { nome: 'Testes Unitários com Jasmine', preco: 'R$ 14,90' },
+    { nome: 'TypeScript na prática', preco: 'R$ 9,90' }
+  ];
 
-    function exibirCard() {
+  function exibirCard() {
 
-        const cards = produtos.map((produto, index) =>
-            <Card key={index}>
-                <Card.Img variant="top" src={placeholder} />
-                <Card.Body className="text-center">
-                    <Card.Title>
-                        {produto.nome}
-                    </Card.Title>
-                    <Card.Text>
-                        Descrição do produto....
-                    </Card.Text>
-                    <Button variant="success" onClick={(event) => { handleComprar(event, produto) }}>
-                        Comprar ({produto.preco})
-                    </Button>
-                </Card.Body>
-            </Card>
+    const cards = produtos.map((produto, index) =>
+      <Card key={index}>
+        <Card.Img variant="top" src={placeholder} />
+        <Card.Body className="text-center">
+          <Card.Title>
+            {produto.nome}
+          </Card.Title>
+          <Card.Text>
+            Descrição do produto....
+          </Card.Text>
+          <Button variant="success" onClick={(event) => { handleComprar(event, produto) }}>
+            Comprar ({produto.preco})
+          </Button>
+        </Card.Body>
+      </Card>
 
-        );
+    );
 
-        return cards;
-    }
+    return cards;
+  }
 
-    function handleComprar(event, produto) {
-        event.preventDefault();
-        // TO DO adicionar ao carrinho
-        // TO DO exibir mensagem
-    }
+  function handleComprar(event, produto) {
+    event.preventDefault();
+    props.adicionarProduto(produto);
+    props.exibirMensagem(produto);
+  }
 
-    return exibirCard();
+  return exibirCard();
+}
+
+ListarProdutos.propTypes = {
+  adicionarProduto: PropTypes.func.isRequired,
+  exibirMensagem: PropTypes.func.isRequired
 }
 
 export default ListarProdutos;
