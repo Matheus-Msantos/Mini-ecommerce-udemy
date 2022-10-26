eb7c278437fde393cc405aa8c1b59aa1832e3af3
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-26 20:08:33 -0300

    style: add index.css

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import MiniEcommerce from './Mini-ecommerce';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
