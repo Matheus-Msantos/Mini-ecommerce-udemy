523f1b7c4c21ba3e67c8664a588376965612fef6
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-28 16:39:16 -0300

    feat: add estrutura do menu

--- front-end/src/components/menu/Menu.js
+++ front-end/src/components/menu/Menu.js
@@ -1,11 +1,53 @@
 import React from 'react';
 
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBasket, faCashRegister, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 function Menu() {
-    return (
-        <>
-            <h1>Menu</h1>
-        </>
-    );
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="">Mini Ecommerce Udemy</Navbar.Brand>
+
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <NavDropdown
+              title={
+                <div style={{ display: 'inline-block' }}>
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                </div>
+              }
+              drop='start'
+            >
+              <NavDropdown.Item href="">
+                <FontAwesomeIcon icon={faShoppingBasket} />
+                Produtos
+              </NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              { /*TO DO Itens do carrinho*/}
+              <NavDropdown.Divider />
+
+              <NavDropdown.Item href="" data-testid="total-carrinho">
+                Total: R$ {/* TO DO Função cálculo do total */}
+              </NavDropdown.Item>
+
+              <span>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">
+                  <FontAwesomeIcon icon={faCashRegister} />
+                  Finalizar compra
+                </NavDropdown.Item>
+              </span>
+
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+    </header>
+  );
 }
 
 export default Menu;
